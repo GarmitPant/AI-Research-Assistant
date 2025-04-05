fcdd6668c3f72611328ac98b07be4c910f56e67a
Garmit Pant <garmitpant@gmail.com>
2025-04-05 17:50:33 +0530

    port binding change

--- backend/main.py
+++ backend/main.py
@@ -45,4 +45,4 @@ async def redirect_to_docs():
     return "/docs"
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
